a409be38b82c1ef803af2a574f3f3244d410d0d9
Sagar Bhowmik <sbhowmik@qti.qualcomm.com>
2018-07-24 18:40:15 -0700

    https://leetcode.com/problems/palindrome-number/description/
    adding the string method

--- palindrome_integer.py
+++ palindrome_integer.py
@@ -24,25 +24,35 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        quotient = temp_x = x
-        rev_x = 0
+        # quotient = temp_x = x
+        # rev_x = 0
+        # if x < 0:
+        #     return False
+        # if 0 < x < 10:
+        #     return True
+        # while quotient != 0:
+        #     quotient = temp_x // 10
+        #     remainder = temp_x % 10
+        #     temp_x = quotient
+        #     rev_x = rev_x * 10 + remainder * 10
+        # if x == rev_x // 10:
+        #     return True
+        # else:
+        #     return False
         if x < 0:
             return False
-        if 0 < x < 10:
+        if x < 10:
             return True
-        while quotient != 0:
-            quotient = temp_x // 10
-            remainder = temp_x % 10
-            temp_x = quotient
-            rev_x = rev_x * 10 + remainder * 10
-        if x == rev_x // 10:
-            return True
-        else:
-            return False
+        str_int = str(x)
+        for i in range(0, len(str_int)/2):
+            if str_int[i] != str_int[len(str_int) - 1 - i]:
+                return False
+            else:
+                continue
+        return True
 
 
-
-integer = 121
+integer = 10
 sol = Solution()
 print(sol.isPalindrome(integer))
 
